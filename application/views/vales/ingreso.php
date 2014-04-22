6da3aec0a889d6793a036e8fa7aeae926865241f
thanf92@gmail.com <thanf92@gmail.com@75b5582d-0c8e-b0dc-48c7-3c59affe3242>
2014-04-22 10:00:46 +0000

    comenzo el diseño de pantallas de vales

--- application/views/vales/ingreso.php
+++ application/views/vales/ingreso.php
@@ -4,46 +4,45 @@
 <form name="formu" id="formu" action="index.php" method="post">
     <p>
         <label for="cantidad" id="lcantidad">Cantidad de Vales: </label>
-        <input type="text" class="tam-1" tabindex="1" id="cantidad" name="cantidad"/> 
+        <input type="text"  size="5" tabindex="1" id="cantidad" name="cantidad"/> 
     </p>
     <p>
         <label for="valor" id="valor">Valor nominal: $</label>
-        <input type="text" class="tam-1" tabindex="1" id="nombre2" name="nombre2"/>
+        <input type="text" size="5" tabindex="1" id="nombre2" name="nombre2"/>
 
     </p >
 
     <p>
         <label for="inicio" id="linicio">Correlativos: Inicio</label>
-        <input class="tam-1" id="inicio" tabindex="2" name="inicio" type="text"/>
+        <input  id="inicio" tabindex="2" name="inicio" type="text" size="5"/>
        <label for="final" id="lfinal">Final</label>
-        <input class="tam-1" id="final" tabindex="2" name="final" type="text"/>
+        <input  size="5" id="final" tabindex="2" name="final" type="text"/>
     </p>
     <p>
         <label for="desingnacion" id="ldesingnacion">Desingnacion</label>
-        <select class="tam-1" tabindex="4" id="desingnacion" name="desingnacion">
+        <select tabindex="4" id="desingnacion" name="desingnacion">
             <option value="1">Servicio General</option>
             <option value="2">Banco Mundial</option>
         </select>
     </p>
     <p>
         <label for="Proveedor" id="lProveedor">Proveedor</label>
-        <select class="tam-1" tabindex="4" id="Proveedor" name="Proveedor">
+        <select tabindex="4" id="Proveedor" name="Proveedor">
             <option value="1">Texaco</option>
             <option value="2">Puma</option>
         </select>
     </p>
     <p>
-        <button type="submit" class="button tam-1 boton_validador" tabindex="8" id="guardar" name="guardar">Guardar</button>
+        <button type="submit"  class="button tam-1 boton_validador" tabindex="8" id="guardar" name="guardar">Guardar</button>
     </p>
 </form>
 <script type="text/javascript">
     $(document).ready(function(){
-       $("#nombre").validacion({
-            valNombre: true
+       $("#cantidad").validacion({
+            min:10
         });
-        $("#nombre2").validacion({
-            alf: true,
-            lonMin: 15
+        $("#valor").validacion({
+			valPrecio: true
         });
         $("#inicio").validacion({
             req: false,
