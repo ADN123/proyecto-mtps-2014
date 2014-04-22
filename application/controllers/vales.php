d43c8d68222a430191eed428459ef48f30599b6b
thanf92@gmail.com <thanf92@gmail.com@75b5582d-0c8e-b0dc-48c7-3c59affe3242>
2014-04-22 16:39:32 +0000

    unido primeras pantallas de vales.php

--- application/controllers/vales.php
+++ application/controllers/vales.php
@@ -15,5 +15,46 @@ class Vales extends CI_Controller
 	{
 		pantalla("vales/ingreso");
   	}
+
+	
+	
+		function requisicion()
+	{
+		
+		$data=$this->seguridad_model->consultar_permiso($this->session->userdata('id_usuario'),59); /*Verificacion de permiso para crear solicitudes*/
+		
+		if($data['id_permiso']!=NULL) {
+			switch($data['id_permiso']) { /*Busqueda de informacion a mostrar en la pantalla segun el nivel del usuario logueado*/
+				case 1:
+					$data['empleados']=$this->transporte_model->consultar_empleado($this->session->userdata('nr'));
+					foreach($data['empleados'] as $val) {
+						$data['info']=$this->transporte_model->info_adicional($val['NR']);
+					}
+					break;
+				case 2:
+					$id_seccion=$this->transporte_model->consultar_seccion_usuario($this->session->userdata('nr'));
+					$data['empleados']=$this->transporte_model->consultar_empleados_seccion($id_seccion['id_seccion']);
+					break;
+				case 3:
+					$data['empleados']=$this->transporte_model->consultar_empleados();
+					break;
+			}
+			$data['estado_transaccion']=$estado_transaccion;
+			//$data['solicitud']=$this->transporte_model->consultar_solicitud($id_solicitud);
+			$data['acompanantes']=$this->transporte_model->consultar_empleados($this->session->userdata('nr'));
+			$data['municipios']=$this->transporte_model->consultar_municipios();
+
+			pantalla('vales/entrega',$data);	
+		}
+		else {
+			echo 'No tiene permisos para acceder';
+		}
+	}
+	
+
+
+	
+
+
 }
 ?>
