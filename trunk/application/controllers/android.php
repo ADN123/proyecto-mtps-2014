22796a08699fb9597173473d986b498c0328672f
thanf92@gmail.com <thanf92@gmail.com@75b5582d-0c8e-b0dc-48c7-3c59affe3242>
2014-05-12 11:46:12 +0000

    Agregadas funciones de logeo para Android

--- trunk/application/controllers/android.php
+++ trunk/application/controllers/android.php
@@ -6,7 +6,7 @@ class Android extends CI_Controller {
         parent::__construct();
 		$this->load->model('android_model');
 		$this->load->model('transporte_model');
-		
+		$this->load->model('seguridad_model');
     }
 
 	
@@ -28,24 +28,100 @@ $v=$this->transporte_model->salidas_entradas_vehiculos();
 		echo $j;
 	}
 	
-	function accesorios(){
+	function accesorios($estado, $id){
 		
-		$j=$this->transporte_model->accesorios();
+		if ($estado==4) {
+			$j=$this->transporte_model->accesoriosABordo($id);			
+		} else {
+			$j=$this->transporte_model->accesorios();			
+		}
+		
+
 		$j=json_encode($j);
 		echo $j;
 		}
 		
-	function registrar(){
-		echo "POST ok:";
-		print_r($_POST);
-		
-		}
+
 	function kilometraje($placa){
 		$j=$this->android_model->kilometraje($placa);
 		$j=json_encode($j);
 		echo $j;
 			
 		}
+
+	function registrar(){
+	
+		$this->db->trans_start();
+			$estado=$this->input->post('estado');
+			$id=$this->input->post('id');
+			$km=$this->input->post('km');
+			$gas=$this->input->post('gas');
+			$hora=date("H:i:s", strtotime($this->input->post('hora')));	
+			/*remuevo de post los datos para que solo queden los accesorios*/
+			$acces=$_POST;
+			unset($acces['estado']);
+			unset($acces['gas']);
+			unset($acces['id']);
+			unset($acces['km']);
+			unset($acces['hora']);			
+			if($estado==3){
+				$this->transporte_model->salida_vehiculo($id, $km,$hora,$acces);
+			}else {				
+				if($estado==4) {
+				$this->transporte_model->regreso_vehiculo($id, $km, $hora, $gas,$acces);					  
+
+				}
+			}
+		$this->db->trans_complete();			
+			echo $this->db->trans_status();	
+		
+		
+		}
+	function iniciar_session(){
+		error_reporting(0);
+
+					$login =$this->input->post('user');
+					$clave =$this->input->post('pass');			
+					$v=$this->seguridad_model->consultar_usuario($login,$clave);  //Verificación en base de datos
+				
+					if($v['id_usuario']==0) {
+						echo 0; //credeciales incorrectas
+
+						
+					}else {
+					$data=$this->seguridad_model->consultar_permiso($v['id_usuario'],67);
+						
+						if($data['id_permiso']!=NULL) {
+								echo 2; //todo bien
+						}else{
+								echo 1; //logeado pero sin permisos
+						}
+					
+
+						
+					//falta verificar si tiene permiso para usar la app movil
+					
+					}	
+			
+	}
+
+	function getSession($login, $clave)
+	{
+		///datos de session 
+	/*
+		$this->session->set_userdata('nombre', $v['nombre_completo']);
+		$this->session->set_userdata('id_usuario', $v['id_usuario']);
+		$this->session->set_userdata('usuario', $v['usuario']);
+		$this->session->set_userdata('nr', $v['NR']);			
+		$this->session->set_userdata('id_seccion', $v['id_seccion']);
+				$login =$this->input->post('user');
+		$clave =$this->input->post('pass');			
+	*/
+
+		$v=$this->seguridad_model->consultar_usuario($login,$clave);  //Verificación en base de datos
+		echo json_encode($v);
+	}
+
 
 }
 ?>
--- trunk/application/views/transporte/despacho.php
+++ trunk/application/views/transporte/despacho.php
@@ -80,25 +80,8 @@
             <br />       
             <fieldset>
                 <legend align='left'>Accesorios</legend>  
-                <table cellspacing='0' align='center' class='table_design'>
-                    <thead>
-                    	<tr>
-                            <th>Objeto</th>
-                            <th width="100">A bordo</th>
-                       	</tr>
-                    </thead>
-                    <tbody>
-                        <?php 
-                            $s="";
-                            foreach($accesorios as $as) {
-                                $s.='<tr title="'.$as->descrip.'" >
-                                    <td>'.$as->nombre.'</td>
-                                    <td align="center"><input type="checkbox"   name="ac'.$as->id_accesorio.'" id="ac'.$as->id_accesorio.'" class="cheke"  value="'.$as->id_accesorio.'"></td>                        </tr>';
-                            }
-                            echo $s;
-                        ?>       
-                    </tbody>
-                </table>
+                <div id="divAccesorios">
+                </div>
                 Seleccionar/Deseleccionar todo <input type="checkbox"  onchange="chekear(this)" >
           	</fieldset>
             <br />
