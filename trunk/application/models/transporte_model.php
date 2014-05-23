fbd3bf5a71fe915b8a6820da6af5a71a95726553
thanf92@gmail.com <thanf92@gmail.com@75b5582d-0c8e-b0dc-48c7-3c59affe3242>
2014-05-23 16:41:27 +0000

    Agregada funcion para saber si es departamental o no

--- trunk/application/models/transporte_model.php
+++ trunk/application/models/transporte_model.php
@@ -111,40 +111,21 @@ class Transporte_model extends CI_Model {
 	function solicitudes_por_confirmar($seccion){
 
 	  $query=$this->db->query("
-		SELECT id_solicitud_transporte id,
-		DATE_FORMAT(fecha_mision,'%d-%m-%Y') fecha,
-		DATE_FORMAT(hora_entrada,'%r') entrada,
-		DATE_FORMAT(hora_salida,'%r') salida,
-		requiere_motorista,
-		LOWER(COALESCE(o.nombre_seccion, 'No hay registro')) seccion,
-		LOWER(CONCAT_WS(' ',s.primer_nombre, s.segundo_nombre, s.tercer_nombre, s.primer_apellido,s.segundo_apellido,s.apellido_casada)) AS nombre
-		FROM tcm_solicitud_transporte  t
-			LEFT JOIN sir_empleado s ON (s.id_empleado=t.id_empleado_solicitante)
-			LEFT JOIN sir_empleado_informacion_laboral i ON (i.id_empleado=s.id_empleado)
-			LEFT JOIN org_seccion o ON (i.id_seccion=o.id_seccion)
-		WHERE (o.id_seccion='$seccion' and estado_solicitud_transporte=1)
-			and (t.id_empleado_solicitante not in
-			(select id_empleado from sir_empleado_informacion_laboral))
-		
-		UNION
-		
-		SELECT id_solicitud_transporte id,
-		DATE_FORMAT(fecha_mision,'%d-%m-%Y') fecha,
-		DATE_FORMAT(hora_entrada,'%r') entrada,
-		DATE_FORMAT(hora_salida,'%r') salida,
-		requiere_motorista,
-		LOWER(COALESCE(o.nombre_seccion, 'No hay registro')) seccion,
-		LOWER(CONCAT_WS(' ',s.primer_nombre, s.segundo_nombre, s.tercer_nombre, s.primer_apellido, s.segundo_apellido, s.apellido_casada)) AS nombre
-		FROM tcm_solicitud_transporte  t
-			LEFT JOIN sir_empleado s ON (s.id_empleado=t.id_empleado_solicitante)
-			LEFT JOIN sir_empleado_informacion_laboral i ON (i.id_empleado=s.id_empleado)
-			LEFT JOIN org_seccion o ON (i.id_seccion=o.id_seccion)
-		WHERE (o.id_seccion='$seccion' and estado_solicitud_transporte=1)
-			and (i.id_empleado_informacion_laboral in
-				(SELECT max(id_empleado_informacion_laboral) as id
-				FROM sir_empleado_informacion_laboral
-				group by id_empleado
-				having count(id_empleado)>=1))
+		SELECT DISTINCT
+					id_solicitud_transporte id,
+					DATE_FORMAT(fecha_mision,'%d-%m-%Y') fecha,
+					DATE_FORMAT(hora_entrada,'%h:%i %p') entrada,
+					DATE_FORMAT(hora_salida,'%h:%i %p') salida,
+					LOWER(CONCAT_WS(' ',e.primer_nombre, e.segundo_nombre, e.tercer_nombre, e.primer_apellido, e.segundo_apellido, e.apellido_casada)) AS nombre,
+					st.estado_solicitud_transporte estado,
+					s.nombre_seccion seccion, i.id_empleado
+					FROM
+					sir_empleado_informacion_laboral AS i
+					LEFT JOIN org_seccion AS s ON s.id_seccion = i.id_seccion
+					LEFT JOIN sir_empleado AS e ON e.id_empleado = i.id_empleado
+					INNER JOIN tcm_solicitud_transporte AS st ON st.id_empleado_solicitante = e.id_empleado
+					WHERE st.estado_solicitud_transporte = 1 AND i.id_seccion= '".$seccion."' AND (i.id_empleado, i.fecha_inicio) IN  
+					( SELECT id_empleado ,MAX(fecha_inicio)  FROM sir_empleado_informacion_laboral GROUP BY id_empleado  ) 
 			");
  
  
@@ -842,7 +823,7 @@ where s.id_solicitud_transporte='$id'");
 							LEFT JOIN sir_empleado AS e ON e.id_empleado = i.id_empleado
 							WHERE (i.id_empleado, i.fecha_inicio) IN  
 							( SELECT id_empleado ,MAX(fecha_inicio)  FROM sir_empleado_informacion_laboral GROUP BY id_empleado  ) 
-							AND i.id_seccion=15
+							AND i.id_seccion=".$id_seccion."
 							ORDER BY e.id_empleado";
 
 
@@ -1142,6 +1123,20 @@ function infoSolicitud($id){
 		$query=$this->db->query($sentencia);
 		
 		return (array)$query->result_array();
+	}
+
+	public function is_departamental($id_seccion=NULL)
+	{	if($id_seccion!=NULL){
+			return false;
+		}
+
+		if($id_seccion>=52 AND $id_seccion<=66 ){
+			return true;
+		}else{
+			return false;
+		}
+
+		
 	}
 
 }
