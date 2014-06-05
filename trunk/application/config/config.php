9e0ae23de7f50c9cf68f36e2aed2773768020588
thanf92@gmail.com <thanf92@gmail.com@75b5582d-0c8e-b0dc-48c7-3c59affe3242>
2014-06-05 16:11:18 +0000

    corregido el dominio.....localhost

--- trunk/application/config/config.php
+++ trunk/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://dorivaldo-pc/mtps/';
+$config['base_url']	= 'http://localhost/mtps/';
 
 /*
 |--------------------------------------------------------------------------
