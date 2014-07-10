bf5d12cad364b2335cb44a2dcab69a95b510343f
thanf92@gmail.com <thanf92@gmail.com@75b5582d-0c8e-b0dc-48c7-3c59affe3242>
2014-07-10 18:51:43 +0000

    Corregido inicio de session

--- trunk/application/views/lock.php
+++ trunk/application/views/lock.php
@@ -1,4 +1,10 @@
 <script src="<?php echo base_url()?>js/views/login.js" type="text/javascript"></script>
+<style>
+	#pie {
+		position: absolute;
+		bottom: 0px;
+	}
+</style>
 
 <div id="contenedor" style=" height:000px;">
 <h1>Sistema Bloqueado</h1>
