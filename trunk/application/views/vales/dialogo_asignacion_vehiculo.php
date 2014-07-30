4fd1a7cf917f52aa752e114b2d6aac8882c52466
thanf92@gmail.com <thanf92@gmail.com@75b5582d-0c8e-b0dc-48c7-3c59affe3242>
2014-07-30 20:11:31 +0000

    antes de reunion

--- trunk/application/views/vales/dialogo_asignacion_vehiculo.php
+++ trunk/application/views/vales/dialogo_asignacion_vehiculo.php
@@ -1,33 +1,45 @@
-<form id='form' action="<?php echo base_url()?>index.php/vales/modificar_asignacion" method='post'>
+<form id='form' action="<?php echo base_url()?>index.php/vales/modificar_asignacion_vehiculo" method='post'>
 
     <fieldset>      
-        <legend align='left'>Información de la General</legend>
+        <legend align='left'>Información del vehículo</legend>
             <?php 
 
                 foreach($d as $datos)
                 {
 
                     $seccion=ucwords($datos['seccion']);
-                    $id_seccion=$datos['id_seccion'];
-                    $cantidad=$datos['cantidad'];
+                    $placa=$datos['placa'];
+                    $modelo=$datos['modelo'];
                     $fuente=$datos['fuente'];
+                    $marca=$datos['marca'];
                     $id_fuente_fondo= $datos['id_fuente_fondo']; 
+                    $id_vehiculo= $datos['id_vehiculo']; 
 
                 }
             
-                echo "Sección: <strong>".$seccion."</strong> <br>
-                Fuente de Fondo: <strong>".$fuente."</strong> <br>
+                echo "Vehiculo a servicio de : <strong>".$seccion."</strong> <br>
+                    Fuente de Fondo: <strong>".$fuente."</strong> <br>
+                    Placa: <strong>".$placa."</strong> <br>
+                    Modelo: <strong>".$modelo."</strong> <br>
+                    Marca: <strong>".$marca."</strong> <br>
                 </fieldset>
     <br />";
 	?>
     <input type='hidden' name='id_seccion' value="<?php echo $id_seccion?>" />
     <input type='hidden' name='id_fuente_fondo' value="<?php echo $id_fuente_fondo?>" />    	
+    <input type='hidden' name='id_vehiculo' value="<?php echo $id_vehiculo?>" />        
 <fieldset>      
-        <legend align='left'>Información adicional</legend>
-    
-        <label for="asignar" id="lasignar" class="tam-2">Cantidad</label>
-        <input class="tam-1" id='asignar' tabindex='2' name='cantidad' type="text"  value="<?php echo $cantidad?>"/>
-    
+  <legend align='left'>Sección de consumo de vehículo</legend>
+<select class="select" style="width:500px;" tabindex="4" id="id_seccion" name="id_seccion" >
+         
+        <?php
+            foreach($oficinas as $val) {
+        ?>
+                <option value="<?php echo $val['id_seccion'] ?>"><?php echo $val['nombre_seccion'] ?></option>
+        <?php   
+            }
+        ?>
+ </select> 
 
  </fieldset>
     <br />
@@ -39,8 +51,5 @@
 </form>
 <script>
 	
-		$("#asignar").validacion({
-			ent: true,
-			numMin: 0
-		});
+		$("#id_seccion").validacion();
 </script>
